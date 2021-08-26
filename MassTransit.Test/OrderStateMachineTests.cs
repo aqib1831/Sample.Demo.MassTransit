6f39928b775ebe6d87619e2084fad4d5eb692d14
Muhammad Aqib <Muhammad.aqib@synergy-it.com>
2021-08-26 11:30:40 +0500

    Test project added and rename Api Project

---
+++ MassTransit.Test/OrderStateMachineTests.cs
@@ -0,0 +1,47 @@
+using MassTransit.Services.StateMachine;
+using MassTransit.Testing;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit.Contracts;
+using Xunit;
+
+namespace MassTransit.Test
+{
+    public class OrderStateMachineTests
+    {
+        [Fact]
+        public async Task Should_create_a_state_instance()
+        {
+            var orderStateMachine = new OrderStateMachine();
+
+            var harness = new InMemoryTestHarness();
+            var saga = harness.StateMachineSaga<OrderState, OrderStateMachine>(orderStateMachine);
+
+            await harness.Start();
+            try
+            {
+                var orderId = NewId.NextGuid();
+
+                await harness.Bus.Publish<OrderSubmittedEvent>(new
+                {
+                    OrderId = orderId,
+                    InVar.Timestamp,
+                    CustomerNumber = "12345"
+                });
+
+                Assert.True(saga.Created.Select(x => x.CorrelationId == orderId).Any());
+
+                var instanceId = await saga.Exists(orderId, x => x.Submitted);
+                Assert.NotNull(instanceId);
+
+                var instance = saga.Sagas.Contains(instanceId.Value);
+                Assert.Equal("12345", instance.CustomerNumber);
+            }
+            finally
+            {
+                await harness.Stop();
+            }
+        }
+    }
+}
